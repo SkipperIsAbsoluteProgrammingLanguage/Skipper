10ba042d7c5df78a934fa668fe9bb33b084d5eda
Danil Salnikov <dzrnl@outlook.com>
2026-01-04 23:48:26 +0300

    refactor: runtime

--- Skipper.Runtime.Tests/GcTests.cs
+++ Skipper.Runtime.Tests/GcTests.cs
@@ -1,3 +1,5 @@
+using Skipper.Runtime.GC;
+using Skipper.Runtime.Memory;
 using Skipper.Runtime.Objects;
 using Skipper.Runtime.Values;
 using Xunit;
@@ -6,217 +8,207 @@ namespace Skipper.Runtime.Tests;
 
 public unsafe class GcTests
 {
-    private readonly TestRootProvider _rootProvider = new();
+    private readonly TestRootProvider _rootProvider;
+    private readonly Heap _heap;
+    private readonly MarkSweepGc _gc;
+
+    public GcTests()
+    {
+        _rootProvider = new TestRootProvider();
+        _heap = new Heap();
+        _gc = new MarkSweepGc(_heap);
+    }
 
     [Fact]
     public void SingleObject_IsCollected_WhenNoRoots()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             []
         );
 
-        rt.Heap.Allocate(desc, 16);
+        _heap.Allocate(desc, 16);
 
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
-        Assert.Empty(rt.Heap.Objects);
+        Assert.Empty(_heap.Objects);
     }
 
     [Fact]
     public void RootObject_IsNotCollected()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             []
         );
 
-        var a = rt.Heap.Allocate(desc, 16);
+        var a = _heap.Allocate(desc, 16);
         _rootProvider.Add(Value.FromObject(a));
 
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
-        Assert.Single(rt.Heap.Objects);
+        Assert.Single(_heap.Objects);
     }
 
     [Fact]
     public void ChainReferences_AreKeptAlive()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             [0]
         );
 
-        var a = rt.Heap.Allocate(desc, sizeof(nint));
-        var b = rt.Heap.Allocate(desc, sizeof(nint));
-        var c = rt.Heap.Allocate(desc, sizeof(nint));
+        var a = _heap.Allocate(desc, sizeof(nint));
+        var b = _heap.Allocate(desc, sizeof(nint));
+        var c = _heap.Allocate(desc, sizeof(nint));
 
         WritePtr(a, b);
         WritePtr(b, c);
 
         _rootProvider.Add(Value.FromObject(a));
-        rt.Collect(_rootProvider);
 
-        Assert.Equal(3, rt.Heap.Objects.Count());
+        _gc.Collect(_rootProvider);
+
+        Assert.Equal(3, _heap.Objects.Count());
     }
 
     [Fact]
     public void UnreachableObject_IsCollected()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             [0]
         );
 
-        var a = rt.Heap.Allocate(desc, sizeof(nint));
-        var b = rt.Heap.Allocate(desc, sizeof(nint));
+        var a = _heap.Allocate(desc, sizeof(nint));
+        var b = _heap.Allocate(desc, sizeof(nint));
 
         WritePtr(a, b);
 
         _rootProvider.Add(Value.FromObject(a));
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
         WritePtr(a, 0);
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
-        Assert.Single(rt.Heap.Objects);
+        Assert.Single(_heap.Objects);
     }
 
     [Fact]
     public void CyclicReferences_WithoutRoots_AreCollected()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             [0]
         );
 
-        var a = rt.Heap.Allocate(desc, sizeof(nint));
-        var b = rt.Heap.Allocate(desc, sizeof(nint));
+        var a = _heap.Allocate(desc, sizeof(nint));
+        var b = _heap.Allocate(desc, sizeof(nint));
 
         WritePtr(a, b);
         WritePtr(b, a);
 
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
-        Assert.Empty(rt.Heap.Objects);
+        Assert.Empty(_heap.Objects);
     }
 
     [Fact]
     public void CyclicReferences_WithRoot_AreKeptAlive()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             [0]
         );
 
-        var a = rt.Heap.Allocate(desc, sizeof(nint));
-        var b = rt.Heap.Allocate(desc, sizeof(nint));
+        var a = _heap.Allocate(desc, sizeof(nint));
+        var b = _heap.Allocate(desc, sizeof(nint));
 
         WritePtr(a, b);
         WritePtr(b, a);
 
         _rootProvider.Add(Value.FromObject(a));
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
-        Assert.Equal(2, rt.Heap.Objects.Count());
+        Assert.Equal(2, _heap.Objects.Count());
     }
 
     [Fact]
     public void PartialGraph_IsCorrectlyCollected()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             [0]
         );
 
-        var a = rt.Heap.Allocate(desc, sizeof(nint));
-        var b = rt.Heap.Allocate(desc, sizeof(nint));
-        rt.Heap.Allocate(desc, sizeof(nint));
+        var a = _heap.Allocate(desc, sizeof(nint));
+        var b = _heap.Allocate(desc, sizeof(nint));
+        _heap.Allocate(desc, sizeof(nint));
 
         WritePtr(a, b);
 
         _rootProvider.Add(Value.FromObject(a));
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
-        Assert.Equal(2, rt.Heap.Objects.Count());
+        Assert.Equal(2, _heap.Objects.Count());
     }
 
     [Fact]
     public void MultipleRoots_AreAllRespected()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             [0]
         );
 
-        var a = rt.Heap.Allocate(desc, sizeof(nint));
-        var b = rt.Heap.Allocate(desc, sizeof(nint));
-        var c = rt.Heap.Allocate(desc, sizeof(nint));
+        var a = _heap.Allocate(desc, sizeof(nint));
+        var b = _heap.Allocate(desc, sizeof(nint));
+        var c = _heap.Allocate(desc, sizeof(nint));
 
         WritePtr(a, c);
 
         _rootProvider.Add(Value.FromObject(a));
         _rootProvider.Add(Value.FromObject(b));
 
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
-        Assert.Equal(3, rt.Heap.Objects.Count());
+        Assert.Equal(3, _heap.Objects.Count());
     }
 
     [Fact]
     public void MultipleCollections_WorkCorrectly()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             []
         );
 
-        var a = rt.Heap.Allocate(desc, 16);
+        var a = _heap.Allocate(desc, 16);
         _rootProvider.Add(Value.FromObject(a));
 
-        rt.Collect(_rootProvider);
-        rt.Collect(_rootProvider);
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
-        Assert.Single(rt.Heap.Objects);
+        Assert.Single(_heap.Objects);
     }
 
     [Fact]
     public void NonObjectValues_AreIgnoredByGC()
     {
-        var rt = new RuntimeContext();
-
         var desc = new ObjectDescriptor(
             ObjectKind.Class,
             []
         );
 
-        rt.Heap.Allocate(desc, 16);
+        _heap.Allocate(desc, 16);
 
         _rootProvider.Add(Value.FromInt(123));
         _rootProvider.Add(Value.FromBool(true));
 
-        rt.Collect(_rootProvider);
+        _gc.Collect(_rootProvider);
 
-        Assert.Empty(rt.Heap.Objects);
+        Assert.Empty(_heap.Objects);
     }
 
     private static void WritePtr(nint target, nint value)
